4113054b4b3dc423b22d263b97c9a68081bc5197
Arda <99423489+ArdaOzturk67@users.noreply.github.com>
2025-05-04 11:35:59 +0300

    part 2

--- project.py
+++ project.py
@@ -45,6 +45,43 @@ m.addConstrs((x.sum('*', j) <= jobs.loc[j, 'Num_Positions'] for j in jobs.index)
 m.optimize()
 Mw = m.ObjVal
 print(f"Maximum total priority weight (Mw) = {Mw}")
-        
+
+
+
+dissimilarity_scores = {}
+priority_weights = jobs['Priority_Weight'].to_dict()
+job_capacities = jobs['Num_Positions'].to_dict()
+
+for (i, j) in allowed_pairs:
+    s_q = np.array(ast.literal_eval(seekers.loc[i, 'Questionnaire']))
+    j_q = np.array(ast.literal_eval(jobs.loc[j, 'Questionnaire']))
+    dij = np.mean(np.abs(s_q - j_q))
+    dissimilarity_scores[(i, j)] = dij
+    
+omega_values = [70, 75, 80, 85, 90, 95, 100]
+results = []
+
+for omega in omega_values:
+    m = gp.Model("Min_Max_Dissimilarity")
+
+    x = m.addVars(allowed_pairs, vtype=gp.GRB.BINARY, name="x")
+    d_max = m.addVar(vtype=gp.GRB.CONTINUOUS, name="d_max")
+
+    m.addConstrs((x[i, j] * dissimilarity_scores[(i, j)] <= d_max for (i, j) in allowed_pairs), name="dissimilarity_bound")
+    m.addConstrs((x.sum(i, '*') <= 1 for i in seekers.index),name="one_job_per_seeker")
+    m.addConstrs((x.sum('*', j) <= jobs.loc[j, 'Num_Positions'] for j in jobs.index), name="job_capacity")
+
+    omega_fraction = omega / 100.0
+    m.addConstr(gp.quicksum(jobs.loc[j, 'Priority_Weight'] * x[i, j] for (i, j) in allowed_pairs) >= omega_fraction * Mw,name="priority_weight_threshold")
+
+    m.setObjective(d_max, gp.GRB.MINIMIZE)
+    m.optimize()
+
+    matched_pairs = [(i, j) for (i, j) in allowed_pairs if x[i, j].X > 0.5] #checks if 0 or 1
+    max_diss = d_max.X if m.status == gp.GRB.OPTIMAL else None
+
+    results.append((omega, max_diss))
+    print(f"ω = {omega}%, Min Max Dissimilarity = {max_diss}")
+    
         
         
